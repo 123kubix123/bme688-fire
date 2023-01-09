f9fdb9b7967f2786877e978095a482d7e12ca759
123kubix123 <kubixwol@gmail.com>
2023-01-09 21:30:35 +0100

    pico code

---
+++ pico-w/bme68x.py
@@ -0,0 +1,36 @@
+import time
+import board
+import busio
+import adafruit_bme680
+import json
+
+class BME68X:
+    def __init__(self, SCL=board.GP1, SDA=board.GP0):
+        # Initialize I2C
+        # busio.I2C(SCL, SDA)
+        self.i2c = busio.I2C(board.GP1, board.GP0)
+
+        # Create BME68X object
+        self.bme68x = adafruit_bme680.Adafruit_BME680_I2C(self.i2c)
+
+        # change this to match the location's pressure (hPa) at sea level
+        # bme68x.sea_level_pressure = 1013.25
+        self.bme68x.sea_level_pressure = 1013
+
+        # You will usually have to add an offset to account for the temperature of
+        # the sensor. This is usually around 5 degrees but varies by use. Use a
+        # separate temperature sensor to calibrate this one.
+        self.temperature_offset = 0
+        self.data = {}
+        
+    def read_data(self):
+        self.data = {
+            'temperature': self.bme68x.temperature + self.temperature_offset,
+            'gas_resistance': self.bme68x.gas,
+            'humidity': self.bme68x.relative_humidity,
+            'pressure': self.bme68x.pressure,
+            #'altitude': self.bme68x.altitude,
+            'air_quality': 0
+            }
+        
+        return self.data
---
+++ pico-w/main.py
@@ -0,0 +1,58 @@
+import network
+import machine
+import secrets
+import time
+try:
+    import usocket as socket
+except:
+    import socket
+#import urequests
+import bme68x
+import json
+
+def blink_onboard_led(num_blinks, delta_t):
+    led = machine.Pin('LED', machine.Pin.OUT)
+    for i in range(num_blinks):
+        led.on()
+        time.sleep(delta_t)
+        led.off()
+        time.sleep(delta_t)
+
+data = {
+    "temperature": 0,
+    "pressure": 0,
+    "humidity": 0,
+    "gas_resistance": 0,
+    "air_quality": 0
+    }
+      
+
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+wlan.connect(secrets.SSID, secrets.PASSWORD)
+blink_onboard_led(5, 0.2)
+print("Is wifi connected? {}".format(wlan.isconnected()))
+
+# Create sensor object
+sensor = bme68x.BME68X()
+
+url = "https://sirserver.dynovski.xyz/update_data"
+
+while True:
+    data = sensor.read_data()
+    time.sleep(3)
+    
+    socketObject = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
+    payload = json.dumps(data)
+    request = "POST /update_data HTTP/1.1\r\nHost: sirserver.dynovski.xyz\r\nContent-Length:"+str(len(payload))+" \r\nContent-Type: application/json \r\n\r\n"+payload+"\r\n\r\n"
+    print(request)
+
+    address = ("192.168.1.64", 16000)
+    socketObject.connect(address)
+    bytessent = socketObject.send(request)
+    socketObject.close()
+    print("Socket closed.")
+    
+
+
